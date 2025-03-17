33afa511dab177a8f0d3275011b64d944314067a
KaranSapra124 <sapra7112@gmail.com>
2025-03-18 00:06:47 +0530

    Added Theme Colors

--- src/Components/Global/Navbar.tsx
+++ src/Components/Global/Navbar.tsx
@@ -24,13 +24,13 @@ const Navbar = () => {
     },
   ]);
   return (
-    <div className="bg-primary-color rounded py-2 px-1 flex justify-between">
-      <h1 className="text-accent-color font-extrabold">HireMeNow</h1>
+    <div className="bg-primary-color items-center  py-2.5 px-1 flex justify-between">
+      <h1 className="text-accent-color text-2xl font-extrabold">HireMeNow</h1>
       <div className="flex ">
         {links?.map((elem: linkInterface, index: number) => {
           return (
             <li
-              className="text-accent-color list-none mx-2 font-semibold"
+              className="text-primary-color p-1 rounded bg-accent-color list-none mx-2 font-semibold"
               key={index}
             >
               {elem?.title}
