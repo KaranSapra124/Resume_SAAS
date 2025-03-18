4599ae2d5732a45d52db19b3e9d90c4d3ac63def
KaranSapra124 <sapra7112@gmail.com>
2025-03-18 23:19:32 +0530

    Started Pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./App.css";
 import Navbar from "./Components/Global/Navbar";
+import Home from "./Pages/Home";
 
 function App() {
   const appRoutes = createBrowserRouter([
-    { element: <Navbar />, path: "/", children: [] },
+    {
+      element: <Navbar />,
+      path: "/",
+      children: [{ element: <Home />, path: "/", children: [] }],
+    },
   ]);
 
   return (
