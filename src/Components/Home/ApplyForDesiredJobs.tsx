5614a51df8c1564af56fa3643c05a0af9698a910
karansapra124 <sapra7112@gmail.com>
2025-03-23 15:08:48 +0530

    Started Resumme Templates

--- src/Components/Home/ApplyForDesiredJobs.tsx
+++ src/Components/Home/ApplyForDesiredJobs.tsx
@@ -1,5 +1,6 @@
 import Container from "../Global/Container";
 import { motion } from "framer-motion";
+import Divider from "../Global/Divider";
 
 const ApplyForDesiredJobs = () => {
   return (
@@ -10,16 +11,18 @@ const ApplyForDesiredJobs = () => {
         transition={{ duration: 1.5 }}
         className="text-center text-white"
       >
-        <h1 className="text-accent-color lg:text-4xl font-bold mb-4">
+        <h1 className="text-accent-color lg:text-4xl font-bold ">
           Job Exploration - Coming Soon! 🚀
         </h1>
+        <Divider className="my-1.5 h-1 rounded-full w-12 bg-accent-color mx-auto" />
+
         <p className="text-lg max-w-2xl mx-auto mb-6">
           We are working on an exciting new feature that will allow you to **discover & apply** 
           for jobs directly from our platform. Stay tuned for updates! 🔥
         </p>
         <button 
           disabled 
-          className="bg-gray-500 text-white px-6 py-3 font-semibold rounded-md cursor-not-allowed"
+          className="border bg-white/20 text-white px-6 py-3 font-semibold rounded-md cursor-not-allowed"
         >
           Coming Soon ⏳
         </button>
